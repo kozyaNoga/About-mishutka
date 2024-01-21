ad375722104f46a0c6b549c36bd1854bdca479b5
Egor <8egor13@inbox.ru>
2024-01-21 22:37:56 +0500

    Shader Creations!

--- About mishutka/Program.cs
+++ About mishutka/Program.cs
@@ -13,6 +13,7 @@ using System.Data;
 using About_mishutka.data.Primitives;
 using About_mishutka.data.Models;
 using About_mishutka.data.Staples;
+using About_mishutka;
 
 namespace ConsoleApp1
 {
@@ -20,6 +21,31 @@ namespace ConsoleApp1
     {
         private float sec = 0.0f;
         private int fps;
+        private readonly float[] _vertices =
+        {
+             0.5f,  0.5f, 0.0f, // top right
+             0.5f, -0.5f, 0.0f, // bottom right
+            -0.5f, -0.5f, 0.0f, // bottom left
+            -0.5f,  0.5f, 0.0f, // top left
+        };
+
+        // Then, we create a new array: indices.
+        // This array controls how the EBO will use those vertices to create triangles
+        private readonly uint[] _indices =
+        {
+            // Note that indices start at 0!
+            0, 3, 1, // The first triangle will be the top-right half of the triangle
+            1, 3, 2  // Then the second will be the bottom-left half of the triangle
+        };
+
+        private int _vertexBufferObject;
+
+        private int _vertexArrayObject;
+        ShaderTK _shader;
+        private int _elementBufferObject;
+
+
+        /*
         Point point = new Point(0.0f, 0.0f, 0.0f);
         Staples staplesLeft = new Staples(new float[]
             { -0.04f+0.83f, 0.0f+0.61f, 0.0f,
@@ -67,6 +93,7 @@ namespace ConsoleApp1
 
         KeyboardState input_coordinat; //Информация о нажатых клавишах.
         private bool KeyPressedOff = true; //индекатор о том, что клавиша была отжата.
+        */
         public Game(GameWindowSettings GWsettings, NativeWindowSettings NWsettings) : base(GWsettings, NWsettings)
         {
             VSync = VSyncMode.On;
@@ -74,9 +101,36 @@ namespace ConsoleApp1
         protected override void OnLoad()
         {
             base.OnLoad();
-            GL.ClearColor(0 / 0.0f, 0 / 0.0f, 0 / 0.0f, 0 / 0.0f);
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
             GL.Enable(EnableCap.CullFace);
             GL.CullFace(CullFaceMode.Back);
+            _vertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+
+            _vertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(_vertexArrayObject);
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            // We create/bind the Element Buffer Object EBO the same way as the VBO, except there is a major difference here which can be REALLY confusing.
+            // The binding spot for ElementArrayBuffer is not actually a global binding spot like ArrayBuffer is. 
+            // Instead it's actually a property of the currently bound VertexArrayObject, and binding an EBO with no VAO is undefined behaviour.
+            // This also means that if you bind another VAO, the current ElementArrayBuffer is going to change with it.
+            // Another sneaky part is that you don't need to unbind the buffer in ElementArrayBuffer as unbinding the VAO is going to do this,
+            // and unbinding the EBO will remove it from the VAO instead of unbinding it like you would for VBOs or VAOs.
+            _elementBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
+            // We also upload data to the EBO the same way as we did with VBOs.
+            GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
+            // The EBO has now been properly setup. Go to the Render function to see how we draw our rectangle now!
+
+            _shader = new ShaderTK("Shaders/Base.vert", "Shaders/Base.frag");
+            _shader.Use();
+
+
+            /*
             point.initVertexBufferObject();
             staplesLeft.initVertexBufferObject();
             staplesRight.initVertexBufferObject();
@@ -84,6 +138,7 @@ namespace ConsoleApp1
             staplesLeftM.initVertexBufferObject();
             line.initVertexBufferObject();
             grid.InitGrid();
+            */
         }
         protected override void OnResize(ResizeEventArgs e)
         {
@@ -105,6 +160,7 @@ namespace ConsoleApp1
             //Наше якобы короткое нажатие слишком длинное.
             //Для того чтобы считывались именно отдельные нажатия была введена переменная KeyPressedOff
             //Говорящая о том, что клавиша была отжата. Так одно нажатие отделяется от другого.
+            /*
             input_coordinat = KeyboardState;
             if (input_coordinat.IsAnyKeyDown && (KeyPressedOff == true))
             {
@@ -113,11 +169,14 @@ namespace ConsoleApp1
             }
            
             if (!input_coordinat.IsAnyKeyDown) KeyPressedOff = true;
+            */
+
             base.OnUpdateFrame(args);
         }
         protected override void OnRenderFrame(FrameEventArgs args)
         {
             GL.Clear(ClearBufferMask.ColorBufferBit);
+            /*
             point.DrawVertexBufferObject(PrimitiveType.TriangleFan);
             staplesLeft.DrawVertexBufferObject(PrimitiveType.LineLoop);
             staplesRight.DrawVertexBufferObject(PrimitiveType.LineLoop);
@@ -125,12 +184,28 @@ namespace ConsoleApp1
             staplesLeftM.DrawVertexBufferObject(PrimitiveType.LineLoop);
             line.DrawVertexBufferObject(PrimitiveType.LineStrip);
             grid.DrawGrid();
+            */
+
+            _shader.Use();
+
+            // Because ElementArrayObject is a property of the currently bound VAO,
+            // the buffer you will find in the ElementArrayBuffer will change with the currently bound VAO.
+            GL.BindVertexArray(_vertexArrayObject);
+
+            // Then replace your call to DrawTriangles with one to DrawElements
+            // Arguments:
+            //   Primitive type to draw. Triangles in this case.
+            //   How many indices should be drawn. Six in this case.
+            //   Data type of the indices. The indices are an unsigned int, so we want that here too.
+            //   Offset in the EBO. Set this to 0 because we want to draw the whole thing.
+            GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
+
             SwapBuffers();
             base.OnRenderFrame(args);
         }
         protected override void OnUnload()
         {
-            
+            _shader.Dispose();
             base.OnUnload();
         }
     }
@@ -156,6 +231,7 @@ namespace ConsoleApp1
             using (Game game = new Game(GameWindowSettings.Default, NWsettings))
             {
                 game.Run();
+
             }
         }
     }
